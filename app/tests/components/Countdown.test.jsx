8fb5705f040d134b2c021dd39df833fcde9ecc25
neonwrx <rolexxx91@gmail.com>
2016-11-17 22:38:35 +0200

    Finalize tests & add page title

--- app/tests/components/Countdown.test.jsx
+++ app/tests/components/Countdown.test.jsx
@@ -33,6 +33,30 @@ describe('Countdown', () => {
 				expect(countdown.state.count).toBe(0);
 				done();
 			}, 3001)
+		});
+
+		it('should pause countdown on paused status', () => {
+			var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+			countdown.handleSetCountdown(3);
+			countdown.handleStatusChange('paused');
+
+			setTimeout(() => {
+				expect(countdown.state.count).toBe(3);
+				expect(countdown.state.countdown).toBe('paused');
+				done();
+			}, 1001);
+		});
+
+		it('should reset count on stopped', () => {
+			var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+			countdown.handleSetCountdown(3);
+			countdown.handleStatusChange('stopped');
+
+			setTimeout(() => {
+				expect(countdown.state.count).toBe(0);
+				expect(countdown.state.countdown).toBe('stopped');
+				done();
+			}, 1001);
 		});		
 	});
 });
